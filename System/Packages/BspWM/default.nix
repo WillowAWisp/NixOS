9376ce3ed855b7955632e80d36bfa3adf41de6d0
Willow Bracker <willow.bracker@gmail.com>
2023-02-21 15:40:15 -0500

    add bspwm

---
+++ System/Packages/BspWM/default.nix
@@ -0,0 +1,3 @@
+{ config, ... }: {
+  services.xserver.windowManager.bspwm.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -14,12 +14,13 @@
       overworld = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./System/Hosts/Overworld/configuration.nix
-          ./System/Hosts/Overworld/hardware.nix
-	  ./System/Packages/Develop
-	  ./System/Packages/Base
-          ./Users/Willow
-          ./Users/Admin
+          ./System/Hosts/Overworld/configuration.nix # System specific configuration
+          ./System/Hosts/Overworld/hardware.nix # System specific configuration
+	  ./System/Packages/Develop # Development packages
+	  ./System/Packages/Base # Base packages
+	  ./System/Packages/BspWM # Going to move over to this, need both atm so not in base
+          ./Users/Willow # My user
+          ./Users/Admin # Root
         ];
         specialArgs = { inherit inputs; };
       };
