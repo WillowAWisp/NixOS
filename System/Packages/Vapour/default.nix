230e70b1065a7404184d6b6460f5350c87ae2c3b
Willow Bracker <willow.bracker@gmail.com>
2023-02-21 16:05:48 -0500

    add packages to Vapour

--- System/Packages/Vapour/default.nix
+++ System/Packages/Vapour/default.nix
@@ -1,3 +1,14 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
+  # Add bspwm.
+  
   services.xserver.windowManager.bspwm.enable = true;
+  
+  # Add polybar, sxhkd, rofi, and feh
+  
+  environment.systemPackages = with pkgs; {
+    polybar
+    sxhkd
+    rofi
+    feh
+  };
 }
