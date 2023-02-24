02f2a021ec138ece87cba2933baa95f4e8a76267
Willow Bracker <willow.bracker@gmail.com>
2023-02-23 22:09:54 -0500

    add btop & pulsemixer

--- System/Packages/Base/default.nix
+++ System/Packages/Base/default.nix
@@ -2,11 +2,13 @@
   # Base packages on all my machines.
   
   environment.systemPackages = with pkgs; [
+    pulsemixer
     killall
     firefox
     steam
     neovim
     discord
     spotify
+    btop
   ];
 }
