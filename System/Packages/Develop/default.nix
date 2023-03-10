671d3e4a54180dd48e70ce91574482b96b310298
Willow Bracker <willow.bracker@gmail.com>
2023-03-09 19:00:42 -0500

    Add JB-Toolbox build file and Add it to Develop package set

--- System/Packages/Develop/default.nix
+++ System/Packages/Develop/default.nix
@@ -1,8 +1,11 @@
 { pkgs, config, ... }: {
   # Dev packages on my machines.
   
+  import ./jb-toolbox
+
   environment.systemPackages = with pkgs; [
     jetbrains.clion
+    callPackage ./jb-toolbox
     clang
     cmake
     lldb
